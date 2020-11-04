a124976dacaf90b1a4064f36d117082af6dcbf7a
Alexander Nilsson <Anilsson2003@hotmail.com>
2020-11-04 11:01:43 +0100

    Spaceship bugfixes, aesthetic changes

--- Spaceinvaders/Spaceinvaders/Game1.cs
+++ Spaceinvaders/Spaceinvaders/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Net.Configuration;
+using System.Threading;
 
 namespace Spaceinvaders
 {
@@ -23,9 +25,13 @@ namespace Spaceinvaders
         Rectangle spaceblock4 = new Rectangle(418, 390, 1, 1);
         Rectangle pew1 = new Rectangle(100, 245, 10, 100);
         Rectangle pew2 = new Rectangle(100, 245, 10, 100);
+        Rectangle pew3 = new Rectangle(100, -150, 10, 85);
+        Rectangle pew4 = new Rectangle(100, -150, 10, 85);
+        Rectangle thicc = new Rectangle(-1, 440, 1000, 1000);
 
         int x_speed = 5;
         int y_speed = 5;
+        int y2_speed = 5;
 
 
 
@@ -152,12 +158,51 @@ namespace Spaceinvaders
                 spaceship1.X = spaceblock3.X - 20;
             if (spaceship1.Intersects(spaceblock3) || spaceship1.Intersects(spaceblock3))
                 spaceship1.X = spaceblock3.X - 20;
-            if (pew1.Y < 0 || pew1.Y > Window.ClientBounds.Height - pew1.Height)
-                pew1.Y = 245;
-            if (pew2.Y < 0 || pew2.Y > Window.ClientBounds.Height - pew2.Height)
-                pew2.Y = 245;
+
+            if (pew3.Intersects(ball)) 
+            {
+                ball = new Rectangle(0, 0, 0, 10);
+            }
+            else if (pew4.Intersects(ball))
+            {
+                ball = new Rectangle(10, 10, 0, 10);
+            }
 
 
+
+            if (pew3.Y == 350)
+            {
+                pew3.Y -= 0;
+            }
+            else
+            {
+                pew3.Y -= y2_speed;
+            }
+
+            if (pew4.Y == 350)
+            {
+                pew4.Y -= 0;
+            }
+            else
+            {
+                pew4.Y -= y2_speed;
+            }
+
+            if (pew3.Y < 0 || pew3.Y > Window.ClientBounds.Height)
+            {
+                pew3.Y = 480;
+            }
+            if (pew4.Y < 0 || pew4.Y > Window.ClientBounds.Height)
+            {
+                pew4.Y = 480;
+            }
+
+                
+            
+            //Task.Delay(200).Wait(pew2.Y = 245);
+            //Task.Delay(200).Wait(pew1.Y = 245);
+            //int milisecounds = 200;
+            //Thread.Sleep(milisecounds);
 
 
 
@@ -180,23 +225,15 @@ namespace Spaceinvaders
             GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
-
             base.Draw(gameTime);
-            double tid = 100;
             spriteBatch.Begin();
             KeyboardState kstate = Keyboard.GetState();
             if (kstate.IsKeyDown(Keys.Space))
             {
-                while (tid > 0)
-                {
-                    spriteBatch.Draw(pixel, pew1, Color.Blue);
-                    spriteBatch.Draw(pixel, pew2, Color.Blue);
-                    pew1.X = spaceship1.X + 5;
-                    pew2.X = spaceship2.X + 5;
-                    pew1.Y -= 1;
-                    pew2.Y -= 1;
-                    tid--;
-                }
+                pew3.Y = 349;
+                pew4.Y = 349;
+                pew3.X = spaceship1.X + 5;
+                pew4.X = spaceship2.X + 5;
 
             }
             else
@@ -205,17 +242,21 @@ namespace Spaceinvaders
                 spriteBatch.Draw(pixel, pew2, Color.Black);
                 pew1.X = spaceship1.X + 5;
                 pew2.X = spaceship2.X + 5;
+                pew3.X = spaceship1.X+5;
+                pew4.X = spaceship2.X+5;
             }
-
-            spriteBatch.Draw(pixel, ball, Color.Green);
+            spriteBatch.Draw(pixel, spaceship1, Color.Green);
+            spriteBatch.Draw(pixel, spaceship2, Color.Green);
             spriteBatch.Draw(pixel, right_paddle, Color.Green);
+            spriteBatch.Draw(pixel, pew3, Color.Blue);
+            spriteBatch.Draw(pixel, pew4, Color.Blue);
+            spriteBatch.Draw(pixel, thicc, Color.Black);
             spriteBatch.Draw(pixel, spaceblock1, Color.Black);
             spriteBatch.Draw(pixel, spaceblock2, Color.Black);
             spriteBatch.Draw(pixel, spaceblock3, Color.Black);
             spriteBatch.Draw(pixel, spaceblock4, Color.Black);
-            spriteBatch.Draw(pixel, spaceship1, Color.Green);
-            spriteBatch.Draw(pixel, spaceship2, Color.Green);
             spriteBatch.Draw(pixel, spaceship3, Color.Green);
+            spriteBatch.Draw(pixel, ball, Color.Green);
 
             spriteBatch.End();
         }
